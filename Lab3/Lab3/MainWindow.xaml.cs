bc06592931d9bbfa9b3bfc1befe7e9c3e7092858
Полуротов Денис <polurotovden@gmail.com>
2020-10-14 20:55:04 +0500

    Создана 3 лаба

---
+++ Lab3/Lab3/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab3
+{
+    public class Organization <AMS>
+    {
+
+    }
+
+    public class Fixed_Wage<T> : Organization <T>
+    {
+
+    }
+
+    public class Hourly<T> : Organization <T>
+    {
+
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+        }
+    }
+}
