9ed763641fa23f0d056fee5b6461ca93b4e9630f
Полуротов Денис <polurotovden@gmail.com>
2020-10-18 01:07:04 +0500

    Проверки прямоугольника, но работает не всегда корректно

--- Lab1/Lab1/MainWindow.xaml.cs
+++ Lab1/Lab1/MainWindow.xaml.cs
@@ -73,9 +73,301 @@ namespace Lab1
             x22 = x + (b / 2);
             y22 = y - (a / 2);
 
-            double[] Square_Arr = { x11, y11, x12, y12, x21, y21, x22, y22 };
+            double[] Rectangle_Arr1 = { x11, y11, x12, y12, x21, y21, x22, y22 };
 
-            return Square_Arr;
+            return Rectangle_Arr1;
+        }
+    }
+
+    public class Intersections
+    {
+        public bool Rectangle_Rectangle(double[] Rectangle1_Arr, double[] Rectangle2_Arr)
+        {
+            bool res = true;
+            double x1 = 0, x2 = 0, x3 = 0, x4 = 0, y1 = 0, y2 = 0, y3 = 0, y4 = 0, k1 = 0, k2 = 0, b1 = 0, b2 = 0, x = 0, y = 0;
+
+            for (int i = 0; i < Rectangle1_Arr.Length; i += 2)
+            {
+                if (i >= 6)
+                {
+                    x1 = Rectangle1_Arr[i];
+                    y1 = Rectangle1_Arr[i + 1];
+                    x2 = Rectangle1_Arr[i - 6];
+                    y2 = Rectangle1_Arr[i - 5];
+                    x3 = Rectangle2_Arr[i];
+                    y3 = Rectangle2_Arr[i + 1];
+                    x4 = Rectangle2_Arr[i - 6];
+                    y4 = Rectangle2_Arr[i - 5];
+                }
+                else
+                {
+                    x1 = Rectangle1_Arr[i];
+                    y1 = Rectangle1_Arr[i + 1];
+                    x2 = Rectangle1_Arr[i + 2];
+                    y2 = Rectangle1_Arr[i + 3];
+                    x3 = Rectangle2_Arr[i];
+                    y3 = Rectangle2_Arr[i + 1];
+                    x4 = Rectangle2_Arr[i + 2];
+                    y4 = Rectangle2_Arr[i + 3];
+                }
+
+                if (x1 >= x2)
+                {
+                    if (i >= 6)
+                    {
+                        x2 = Rectangle1_Arr[i];
+                        y2 = Rectangle1_Arr[i + 1];
+                        x1 = Rectangle1_Arr[i - 6];
+                        y1 = Rectangle1_Arr[i - 5];
+                    }
+                    else
+                    {
+                        x2 = Rectangle1_Arr[i];
+                        y2 = Rectangle1_Arr[i + 1];
+                        x1 = Rectangle1_Arr[i + 2];
+                        y1 = Rectangle1_Arr[i + 3];
+                    }
+                }
+                else
+                {
+                    if (i >= 6)
+                    {
+                        x1 = Rectangle1_Arr[i];
+                        y1 = Rectangle1_Arr[i + 1];
+                        x2 = Rectangle1_Arr[i - 6];
+                        y2 = Rectangle1_Arr[i - 5];
+                    }
+                    else
+                    {
+                        x1 = Rectangle1_Arr[i];
+                        y1 = Rectangle1_Arr[i + 1];
+                        x2 = Rectangle1_Arr[i + 2];
+                        y2 = Rectangle1_Arr[i + 3];
+                    }
+                }
+
+                if (x3 >= x4)
+                {
+                    if (i >= 6)
+                    {
+                        x4 = Rectangle2_Arr[i];
+                        y4 = Rectangle2_Arr[i + 1];
+                        x3 = Rectangle2_Arr[i - 6];
+                        y3 = Rectangle2_Arr[i - 5];
+                    }
+                    else
+                    {
+                        x4 = Rectangle2_Arr[i];
+                        y4 = Rectangle2_Arr[i + 1];
+                        x3 = Rectangle2_Arr[i + 2];
+                        y3 = Rectangle2_Arr[i + 3];
+                    }
+                }
+                else
+                {
+                    if (i >= 6)
+                    {
+                        x3 = Rectangle2_Arr[i];
+                        y3 = Rectangle2_Arr[i + 1];
+                        x4 = Rectangle2_Arr[i - 6];
+                        y4 = Rectangle2_Arr[i - 5];
+                    }
+                    else
+                    {
+                        x3 = Rectangle2_Arr[i];
+                        y3 = Rectangle2_Arr[i + 1];
+                        x4 = Rectangle2_Arr[i + 2];
+                        y4 = Rectangle2_Arr[i + 3];
+                    }
+                }
+
+                if (y1 == y2)
+                {
+                    k1 = 0;
+                }
+                else
+                {
+                    k1 = (y2 - y1) / (x2 - x1);
+                }
+
+                if (y3 == y4)
+                {
+                    k2 = 0;
+                }
+                else
+                {
+                    k2 = (y4 - y3) / (x4 - x3);
+                }
+
+                if (k1 == k2)
+                {
+                    res = false;
+                }
+
+                else
+                {
+                    b1 = y1 - (k1 * x1);
+                    b2 = y3 - (k2 * x3);
+
+                    x = (b2 - b1) / (k1 - k2);
+                    y = (k1 * x) + b1;
+
+                    if (((x1 <= x4) && (x4 <= x2)) || ((x1 <= x3) && (x3 <= x2)))
+                    {
+                        res = true;
+                    }
+                }
+            }
+            return res;
+        }
+
+        public bool Rectangle_Square(double[] Rectangle1_Arr, double[] Square2_Arr)
+        {
+            bool res = false;
+            double x1, x2, x3, x4, y1, y2, y3, y4, k1, k2, b1, b2, x, y;
+
+            for (int i = 0; i < Rectangle1_Arr.Length; i += 2)
+            {
+                if (i >= 6)
+                {
+                    x1 = Rectangle1_Arr[i];
+                    y1 = Rectangle1_Arr[i + 1];
+                    x2 = Rectangle1_Arr[i - 6];
+                    y2 = Rectangle1_Arr[i - 5];
+                    x3 = Square2_Arr[i];
+                    y3 = Square2_Arr[i + 1];
+                    x4 = Square2_Arr[i - 6];
+                    y4 = Square2_Arr[i - 5];
+                }
+                else
+                {
+                    x1 = Rectangle1_Arr[i];
+                    y1 = Rectangle1_Arr[i + 1];
+                    x2 = Rectangle1_Arr[i + 2];
+                    y2 = Rectangle1_Arr[i + 3];
+                    x3 = Square2_Arr[i];
+                    y3 = Square2_Arr[i + 1];
+                    x4 = Square2_Arr[i + 2];
+                    y4 = Square2_Arr[i + 3];
+                }
+
+                if (x1 >= x2)
+                {
+                    if (i >= 6)
+                    {
+                        x2 = Rectangle1_Arr[i];
+                        y2 = Rectangle1_Arr[i + 1];
+                        x1 = Rectangle1_Arr[i - 6];
+                        y1 = Rectangle1_Arr[i - 5];
+                    }
+                    else
+                    {
+                        x2 = Rectangle1_Arr[i];
+                        y2 = Rectangle1_Arr[i + 1];
+                        x1 = Rectangle1_Arr[i + 2];
+                        y1 = Rectangle1_Arr[i + 3];
+                    }
+                }
+                else
+                {
+                    if (i >= 6)
+                    {
+                        x1 = Rectangle1_Arr[i];
+                        y1 = Rectangle1_Arr[i + 1];
+                        x2 = Rectangle1_Arr[i - 6];
+                        y2 = Rectangle1_Arr[i - 5];
+                    }
+                    else
+                    {
+                        x1 = Rectangle1_Arr[i];
+                        y1 = Rectangle1_Arr[i + 1];
+                        x2 = Rectangle1_Arr[i + 2];
+                        y2 = Rectangle1_Arr[i + 3];
+                    }
+                }
+
+                if (x3 >= x4)
+                {
+                    if (i >= 6)
+                    {
+                        x4 = Square2_Arr[i];
+                        y4 = Square2_Arr[i + 1];
+                        x3 = Square2_Arr[i - 6];
+                        y3 = Square2_Arr[i - 5];
+                    }
+                    else
+                    {
+                        x4 = Square2_Arr[i];
+                        y4 = Square2_Arr[i + 1];
+                        x3 = Square2_Arr[i + 2];
+                        y3 = Square2_Arr[i + 3];
+                    }
+                }
+                else
+                {
+                    if (i >= 6)
+                    {
+                        x3 = Square2_Arr[i];
+                        y3 = Square2_Arr[i + 1];
+                        x4 = Square2_Arr[i - 6];
+                        y4 = Square2_Arr[i - 5];
+                    }
+                    else
+                    {
+                        x3 = Square2_Arr[i];
+                        y3 = Square2_Arr[i + 1];
+                        x4 = Square2_Arr[i + 2];
+                        y4 = Square2_Arr[i + 3];
+                    }
+                }
+
+                if (y1 == y2)
+                {
+                    k1 = 0;
+                }
+                else
+                {
+                    k1 = (y2 - y1) / (x2 - x1);
+                }
+
+                if (y3 == y4)
+                {
+                    k2 = 0;
+                }
+                else
+                {
+                    k2 = (y4 - y3) / (x4 - x3);
+                }
+
+                if (k1 == k2)
+                {
+                    res = false;
+                }
+                else
+                {
+                    b1 = y1 - (k1 * x1);
+                    b2 = y3 - (k2 * x3);
+
+                    x = (b2 - b1) / (k1 - k2);
+                    y = (k1 * x) + b1;
+
+                    if (((x1 <= x4) && (x4 <= x2)) || ((x1 <= x3) && (x3 <= x2)))
+                    {
+                        res = true;
+                    }
+                }
+            }
+            return res;
+        }
+
+        public bool Rectangle_Circle()
+        {
+            return false;
+        }
+
+        public bool Circle_Square()
+        {
+            return false;
         }
     }
 
@@ -99,6 +391,10 @@ namespace Lab1
 
         private void Go_Click(object sender, RoutedEventArgs e)
         {
+            Rectangle rectangle = new Rectangle();
+            Square square = new Square();
+            Intersections intersections = new Intersections();
+
             if (Circle1.IsChecked == true)
             {
                 if (Circle2.IsChecked == true)
@@ -117,14 +413,39 @@ namespace Lab1
             }
             else if (Rectangle1.IsChecked == true)
             {
-                Square square = new Square();
+                double[] Rectangle1_Arr = rectangle.Rectangle_Create(x, y, a, b);
 
-                x = Convert.ToDouble(xQ1.Text);
-                y = Convert.ToDouble(yQ1.Text);
-                a = Convert.ToDouble(aQ1.Text);
-                b = Convert.ToDouble(bQ1.Text);
+                if (Rectangle2.IsChecked == true)
+                {
+                    x = Convert.ToDouble(xQ2.Text);
+                    y = Convert.ToDouble(yQ2.Text);
+                    a = Convert.ToDouble(aQ2.Text);
+                    b = Convert.ToDouble(bQ2.Text);
 
-                square.Square_Create(x, y, a);
+                    double[] Rectangle2_Arr = rectangle.Rectangle_Create(x, y, a, b);
+
+                    bool res = intersections.Rectangle_Rectangle(Rectangle1_Arr, Rectangle2_Arr);
+
+                    if (res == true)
+                        Answer.Text = Convert.ToString("Ответ: Пересекаются");
+                    else
+                        Answer.Text = Convert.ToString("Ответ: Не пересекаются");
+                }
+                else if (Square2.IsChecked == true)
+                {
+                    x = Convert.ToDouble(xQ2.Text);
+                    y = Convert.ToDouble(yQ2.Text);
+                    a = Convert.ToDouble(aQ2.Text);
+
+                    double[] Square2_Arr = square.Square_Create(x, y, a);
+
+                    bool res = intersections.Rectangle_Square(Rectangle1_Arr, Square2_Arr);
+
+                    if (res == true)
+                        Answer.Text = Convert.ToString("Ответ: Пересекаются");
+                    else
+                        Answer.Text = Convert.ToString("Ответ: Не пересекаются");
+                }
             }
             else if (Square1.IsChecked == true)
             {
